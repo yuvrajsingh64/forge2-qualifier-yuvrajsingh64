d5bf85716f5721f53d6e95777103cd27d96624c7
yuvrajsingh64 <yuvrajsingh64@github.com>
2026-06-25 21:50:11 +0530

    fix cors: move handlecors to global middleware stack

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,9 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            HandleCors::class,
-        ]);
+        $middleware->prepend(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
